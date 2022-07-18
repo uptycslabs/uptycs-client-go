35123d72a6afe18161655caa069cc39ab3c162c3
Marcus Young <myoung34@my.apsu.edu>
2022-07-18 16:06:54 -0500

    Dont use omitempty on user updateable bools

--- uptycs/models.go
+++ uptycs/models.go
@@ -337,7 +337,7 @@ type User struct {
 	Name                string   `json:"name,omitempty"`
 	Email               string   `json:"email,omitempty"`
 	Phone               string   `json:"phone,omitempty"`
-	Active              bool     `json:"active,omitempty"`
+	Active              bool     `json:"active"`
 	SuperAdmin          bool     `json:"superAdmin,omitempty"`
 	Bot                 bool     `json:"bot,omitempty"`
 	Support             bool     `json:"support,omitempty"`
